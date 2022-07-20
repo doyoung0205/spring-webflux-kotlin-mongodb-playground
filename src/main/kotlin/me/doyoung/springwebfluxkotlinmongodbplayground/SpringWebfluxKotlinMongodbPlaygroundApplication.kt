be39bf2d59597555442beca46dacfa798dfa47bb
doyoung0205 <doyoung0205@naver.com>
2022-07-20 23:45:21 +0900

    first commit

---
+++ src/main/kotlin/me/doyoung/springwebfluxkotlinmongodbplayground/SpringWebfluxKotlinMongodbPlaygroundApplication.kt
@@ -0,0 +1,11 @@
+package me.doyoung.springwebfluxkotlinmongodbplayground
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringWebfluxKotlinMongodbPlaygroundApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringWebfluxKotlinMongodbPlaygroundApplication>(*args)
+}
---
+++ src/test/kotlin/me/doyoung/springwebfluxkotlinmongodbplayground/SpringWebfluxKotlinMongodbPlaygroundApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.doyoung.springwebfluxkotlinmongodbplayground
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringWebfluxKotlinMongodbPlaygroundApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
